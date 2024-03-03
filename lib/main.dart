dccc5f6146f0cea17a5a9c06c11a730ca2508ef5
My Name <zhylkybaev.salamat.com>
2024-03-03 22:36:19 +0100

    MyHomePage change title name

--- lib/main.dart
+++ lib/main.dart
@@ -10,12 +10,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Change Color App',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Color Change App'),
     );
   }
 }
